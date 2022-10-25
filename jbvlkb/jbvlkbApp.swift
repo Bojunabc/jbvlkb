bf8512a1b0ee57cad40de922e2601580be2f503b
Roy <a0966750212@icloud.com>
2022-10-25 20:55:17 +0800

    Initial Commit

---
+++ jbvlkb/jbvlkbApp.swift
@@ -0,0 +1,17 @@
+//
+//  jbvlkbApp.swift
+//  jbvlkb
+//
+//  Created by 暨大附中 on 2022/10/25.
+//
+
+import SwiftUI
+
+@main
+struct jbvlkbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
